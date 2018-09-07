42ba39c33c8b51d205ee3132cc9bf8ab24bc955e
eveporcello <eve@moonhighway.com>
2018-09-06 22:05:40 -0700

    Hello World Apollo Server

---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server')
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+const resolvers = {
+    Query: {
+        hello: () => 'hello world!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server running at ${url}`)
+})

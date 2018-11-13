31f13fbba920894e2577c68230cc79ed8698d722
eveporcello <eve@moonhighway.com>
2018-11-12 17:59:30 -0800

    adds trail fix

--- index.js
+++ index.js
@@ -60,90 +60,86 @@ const typeDefs = `
     }
 `
 const resolvers = {
-    Query: {
-        allLifts: (root, { status }, { lifts }) => {
-            if (!status) {
-                return lifts
-            } else {
-                var filteredLifts = lifts.filter(lift => lift.status === status)
-                return filteredLifts
-            }
-        },
-        Lift: (root, { id }, { lifts }) => {
-            var selectedLift = lifts.filter(lift => id === lift.id)
-            return selectedLift[0]
-        },
-        liftCount: (root, { status }, { lifts }) => {
-            var i = 0
-            lifts.map(lift => {
-                lift.status === status ?
-                    i++ :
-                    null
-            })
-            return i
-        },
-        allTrails: (root, { status }, { trails }) => {
-            if (!status) {
-                return trails
-            } else {
-                var filteredTrails = trails.filter(trail => trail.status === status)
-                return filteredTrails
-            }
-        },
-        Trail: (root, { id }, { trails }) => {
-            var selectedTrail = trails.filter(trail => id === trail.id)
-            return selectedTrail[0]
-        },
-        trailCount: (root, { status }, { trails }) => {
-            var i = 0
-            trails.map(trail => {
-                trail.status === status ?
-                    i++ :
-                    null
-            })
-            return i
-        }
+  Query: {
+    allLifts: (root, { status }, { lifts }) => {
+      if (!status) {
+        return lifts
+      } else {
+        let filteredLifts = lifts.filter(lift => lift.status === status)
+        return filteredLifts
+      }
     },
-    Mutation: {
-        setLiftStatus: (root, { id, status }, { lifts, pubsub }) => {
-            var updatedLift = lifts.find(lift => id === lift.id)
-            updatedLift.status = status
-            pubsub.publish('lift-status-change', { liftStatusChange: updatedLift })
-            return updatedLift
-        },
-        setTrailStatus: (root, { id, status }, { trails, pubsub }) => {
-            var updatedTrail = trails.find(trail => id === trail.id)
-            updatedTrail.status = status
-            pubsub.publish('trail-status-change', { trailStatusChange: updatedTrail })
-            return updatedTrail
-        }
+    Lift: (root, { id }, { lifts }) => {
+      let selectedLift = lifts.find(lift => id === lift.id)
+      return selectedLift[0]
     },
-    Subscription: {
-        liftStatusChange: {
-            subscribe: (root, data, { pubsub }) => pubsub.asyncIterator('lift-status-change')
-        },
-        trailStatusChange: {
-            subscribe: (root, data, { pubsub }) => pubsub.asyncIterator('trail-status-change')
-        }
+    liftCount: (root, { status }, { lifts }) => {
+      let i = 0
+      lifts.map(lift => {
+        lift.status === status ? i++ : null
+      })
+      return i
     },
-    Lift: {
-        trailAccess: (root, args, { trails }) => root.trails
-            .map(id => trails.find(t => id === t.id))
-            .filter(x => x)
+    allTrails: (root, { status }, { trails }) => {
+      if (!status) {
+        return trails
+      } else {
+        let filteredTrails = trails.filter(trail => trail.status === status)
+        return filteredTrails
+      }
     },
-    Trail: {
-        accessedByLifts: (root, args, { lifts }) => root.lift
-            .map(id => lifts.find(l => id === l.id))
-            .filter(x => x)
+    Trail: (root, { id }, { trails }) => {
+      let selectedTrail = trails.filter(trail => id === trail.id)
+      return selectedTrail[0]
+    },
+    trailCount: (root, { status }, { trails }) => {
+      let i = 0
+      trails.map(trail => {
+        trail.status === status ? i++ : null
+      })
+      return i
     }
+  },
+  Mutation: {
+    setLiftStatus: (root, { id, status }, { lifts, pubsub }) => {
+      let updatedLift = lifts.find(lift => id === lift.id)
+      updatedLift.status = status
+      pubsub.publish('lift-status-change', { liftStatusChange: updatedLift })
+      return updatedLift
+    },
+    setTrailStatus: (root, { id, status }, { trails, pubsub }) => {
+      let updatedTrail = trails.find(trail => id === trail.id)
+      updatedTrail.status = status
+      pubsub.publish('trail-status-change', { trailStatusChange: updatedTrail })
+      return updatedTrail
+    }
+  },
+  Subscription: {
+    liftStatusChange: {
+      subscribe: (root, data, { pubsub }) =>
+        pubsub.asyncIterator('lift-status-change')
+    },
+    trailStatusChange: {
+      subscribe: (root, data, { pubsub }) =>
+        pubsub.asyncIterator('trail-status-change')
+    }
+  },
+  Lift: {
+    trailAccess: (root, args, { trails }) =>
+      root.trails.map(id => trails.find(t => id === t.id)).filter(x => x)
+  },
+  Trail: {
+    accessedByLifts: (root, args, { lifts }) =>
+      root.lift.map(id => lifts.find(l => id === l.id)).filter(x => x)
+  }
 }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context
+  typeDefs,
+  resolvers,
+  context
 })
 
 server.listen().then(({ url }) => {
-    console.log(`Server running at ${url}`)
+  console.log(`Server running at ${url}`)
 })
